bc4a29e0ad7e5315b6019b29e751c05639cc9273
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-24 19:49:02 +0900

    add devtools

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.0")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
