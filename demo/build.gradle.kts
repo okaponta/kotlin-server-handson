7525eb9146ac47bfcafb42d06db174923a5a84f0
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-24 18:34:35 +0900

    version up

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -16,12 +16,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:2.7.0")
+	implementation("org.springframework.boot:spring-boot-starter-web:2.7.0")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.0")
 }
 
 tasks.withType<KotlinCompile> {
