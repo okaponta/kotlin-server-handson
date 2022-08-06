7c5621b8bb6b66e0ef41dad79c1b4caf03e8374a
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-06 15:25:33 +0900

    setup mysql

---
+++ book-manager/db/sql/init.sql
@@ -0,0 +1,7 @@
+insert into book values
+(100, 'Kotlin入門', 'コトリン太郎', '1950-10-01'),
+(200, 'Java入門', 'ジャヴァ太郎', '2005-08-29');
+
+insert into user values
+(1, 'admin@test.com', '$2y$05$Uap5mteK/0vd/Whv2DWf3uco2x2asD7DTg0SiDoXmdJ.mvFNETi.K', '管理者', 'admin'),
+(2, 'user@test.com', '$2y$05$Uap5mteK/0vd/Whv2DWf3uco2x2asD7DTg0SiDoXmdJ.mvFNETi.K', 'ユーザー', 'USER');
