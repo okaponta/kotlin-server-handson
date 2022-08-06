3f92849934b2fab858ddaf0158f5fc7bef3ef7b1
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-06 15:25:23 +0900

    add dependencies to gradle

--- book-manager/build.gradle.kts
+++ book-manager/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.2"
 	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+	id("com.thinkimi.gradle.MybatisGenerator") version "2.4"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -20,7 +21,12 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.4.0")
+	implementation("mysql:mysql-connector-java:8.0.29")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -33,4 +39,9 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+mybatisGenerator {
+	verbose = true
+	configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
 }
