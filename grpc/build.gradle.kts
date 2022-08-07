f64ec35e6a216fdeeb44c573bda39ff526cbddfb
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-07 23:41:17 +0900

    add gradle project

---
+++ grpc/build.gradle.kts
@@ -0,0 +1,70 @@
+import com.google.protobuf.gradle.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.2"
+	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+	id("com.google.protobuf") version "0.8.19"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("com.google.protobuf:protobuf-java:3.21.4")
+	implementation("io.grpc:grpc-protobuf:1.48.1")
+	implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+	implementation("io.grpc:grpc-netty:1.48.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
+
+	implementation("io.github.lognet:grpc-spring-boot-starter:4.8.0")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.21.4"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java:1.36.0"
+		}
+		id("grpckt") {
+			artifact = "io.grpc:protoc-gen-grpc-kotlin:1.3.0:jdk8@jar"
+		}
+	}
+	generateProtoTasks {
+		all().forEach {
+			it.plugins {
+				id("grpc")
+				id("grpckt")
+			}
+		}
+	}
+	generatedFilesBaseDir = "src/main/kotlin/com/example/grpc/"
+}
