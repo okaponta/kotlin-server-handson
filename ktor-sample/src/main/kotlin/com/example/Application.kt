4cff5574b329b564e544f273211ec2d9d25c658e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-08 19:36:58 +0900

    add path param and query param

--- ktor-sample/src/main/kotlin/com/example/Application.kt
+++ ktor-sample/src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,8 @@
 package com.example
 
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
-import io.ktor.http.*
-import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -14,14 +12,14 @@ fun main() {
             get("/") {
                 call.respondText("Hello Ktor!")
             }
+            get("/hello/{name}") {
+                val name = call.parameters["name"]
+                call.respondText("Hello $name!")
+            }
+            get("/hello") {
+                val name = call.request.queryParameters["name"]
+                call.respondText("Hello $name!")
+            }
         }
     }.start(wait = true)
-}
-
-fun Application.module(testing: Boolean = false) {
-    routing {
-        get("/") {
-            call.respondText("Hello Ktor!")
-        }
-    }
 }
