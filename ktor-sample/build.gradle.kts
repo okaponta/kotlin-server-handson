907fbb1b9bba6b8738372d173ba7e014efd2b434
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-08 20:05:54 +0900

    add path with location

--- ktor-sample/build.gradle.kts
+++ ktor-sample/build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-locations:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- ktor-sample/src/main/kotlin/com/example/Application.kt
+++ ktor-sample/src/main/kotlin/com/example/Application.kt
@@ -2,14 +2,17 @@ package com.example
 
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.locations.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        install(Locations)
         routing {
             greetingRoute()
+            userRoute()
             get("/hello/{name}") {
                 val name = call.parameters["name"]
                 call.respondText("Hello $name!")
@@ -31,4 +34,24 @@ fun Routing.greetingRoute() {
             call.respondText("Good morning!")
         }
     }
+}
+
+fun Routing.userRoute() {
+    get<UserLocation.GetUserLocation> { param ->
+        val id = param.id
+        call.respondText("id=$id")
+    }
+    get<UserLocation.GetDetailLocation> { param ->
+        val id = param.id
+        call.respondText("getDetail id=$id")
+    }
+}
+
+@Location("/user")
+class UserLocation {
+    @Location("/{id}")
+    data class GetUserLocation(val id: Long)
+
+    @Location("/detail/{id}")
+    data class GetDetailLocation(val id: Long)
 }
